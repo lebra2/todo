9ef124eb539683926e2f5bbe00e73ee552976894
lebra2 <92708848+lebra2@users.noreply.github.com>
2022-11-30 09:27:39 +0200

    tasks

--- functions.php
+++ functions.php
@@ -2,10 +2,10 @@
 
 function connectToDb () {
 
-    $host = '';
-    $db = '';
-    $user = '';
-    $passwd = '';
+    $host = 'd107230.mysql.zonevs.eu';
+    $db = 'd107230_todo';
+    $user = 'd107230sa421002';
+    $passwd = 'Tak21Salas6na';
     $charset = 'utf8mb4';
     
     try {
